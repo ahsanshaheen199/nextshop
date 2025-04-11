89b13264a2226abf0921a7fe3139025119eaaaa1
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2025-04-11 22:23:45 +0600

    feat: add footer responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,25 +9,25 @@ import { Envelop } from "./icons/envelop";
 export function Footer() {
   return (
     <footer>
-      <div className="container mx-auto mb-[-90px] relative z-[1]">
-        <div className="bg-black rounded-[20px] py-9 px-16 grid grid-cols-12 items-center">
-          <div className="col-start-1 col-end-7">
-            <h2 className="text-[40px] leading-[45px] text-white uppercase">
+      <div className="container mb-[-90px] relative z-[1]">
+        <div className="bg-black rounded-[20px] px-6 py-8 lg:py-9 lg:px-16 grid grid-cols-12 items-center">
+          <div className="lg:col-start-1 lg:col-end-8 col-span-12">
+            <h2 className="lg:text-[40px] lg:leading-[45px] text-[32px] leading-[35px] text-white uppercase">
               Stay Upto Date About Our Latest Offers
             </h2>
           </div>
-          <div className="col-start-9 col-end-13">
+          <div className="lg:col-start-9 lg:col-end-13 col-span-12 mt-8 lg:mt-0">
             <form>
               <div className="relative">
                 <Envelop className="absolute top-1/2 left-4 -translate-y-1/2 fill-black/40" />
                 <input
                   type="email"
-                  className="bg-white w-full rounded-full text-base placeholder:text-black/40 pl-[52px] py-3 pr-4 text-black outline-none"
+                  className="bg-white w-full rounded-full text-sm lg:text-base placeholder:text-black/40 lg:pl-[52px] pl-12 py-3 pr-4 text-black outline-none"
                   placeholder="Enter your email address"
                 />
               </div>
               <div className="mt-[14px]">
-                <button className="w-full block bg-white rounded-full text-base font-satoshi-medium py-3 text-center cursor-pointer hover:bg-black hover:text-white transition-colors duration-300 ease-in-out border border-black/20 hover:border-white">
+                <button className="w-full block bg-white rounded-full text-sm lg:text-base font-satoshi-medium py-3 text-center cursor-pointer hover:bg-black hover:text-white transition-colors duration-300 ease-in-out border border-black/20 hover:border-white">
                   Subscribe to Newsletter
                 </button>
               </div>
@@ -36,13 +36,13 @@ export function Footer() {
         </div>
       </div>
       <div className="bg-[#F0F0F0] pt-[140px] py-14">
-        <div className="container mx-auto">
-          <div className="flex justify-between mb-12">
-            <div className="max-w-64">
+        <div className="container">
+          <div className="grid grid-cols-12 mb-12 gap-y-6 lg:gap-y-0">
+            <div className="lg:col-span-4 md:col-span-6 md:max-w-64 col-span-12">
               <h1 className="text-3xl font-bold mb-6 leading-[1]">NextShop</h1>
               <p className="text-sm text-black/60 leading-[22px] mb-6">
                 We have cloths that suits your style and which you&apos;re proud
-                to wear. From men to women;
+                to wear. From men to women
               </p>
               <ul className="flex items-center gap-x-3">
                 <li>
@@ -79,7 +79,7 @@ export function Footer() {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="lg:col-span-2 col-span-6">
               <h3 className="font-satoshi-medium tracking-[3px] text-base leading-[18px] text-black mb-6 uppercase">
                 Company
               </h3>
@@ -98,7 +98,7 @@ export function Footer() {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="lg:col-span-2 col-span-6">
               <h3 className="font-satoshi-medium tracking-[3px] text-base leading-[18px] text-black mb-6 uppercase">
                 Help
               </h3>
@@ -117,7 +117,7 @@ export function Footer() {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="lg:col-span-2 col-span-6">
               <h3 className="font-satoshi-medium tracking-[3px] text-base leading-[18px] text-black mb-6 uppercase">
                 Faq
               </h3>
@@ -136,7 +136,7 @@ export function Footer() {
                 </li>
               </ul>
             </div>
-            <div>
+            <div className="lg:col-span-2 col-span-6">
               <h3 className="font-satoshi-medium tracking-[3px] text-base leading-[18px] text-black mb-6 uppercase">
                 Resources
               </h3>
@@ -156,13 +156,13 @@ export function Footer() {
               </ul>
             </div>
           </div>
-          <div className="grid grid-cols-2 items-center border-t border-black/10 pt-6">
-            <div>
+          <div className="md:grid md:grid-cols-2 md:items-center border-t border-black/10 pt-6">
+            <div className="text-center md:text-left">
               <p className="text-sm text-black/60">
                 NextShop {new Date().getFullYear()}. All Rights Reserved.
               </p>
             </div>
-            <div className="flex justify-end items-center">
+            <div className="flex md:justify-end justify-center mt-4 md:mt-0 items-center">
               <Image width={50} height={40} src="/visa.svg" alt="visa" />
               <Image width={50} height={40} src="/mastercard.svg" alt="visa" />
               <Image width={50} height={40} src="/paypal.svg" alt="visa" />
