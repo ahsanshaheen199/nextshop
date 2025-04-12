705bc1ddb727c4196f983441a781641ce61f0d47
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2025-04-12 17:11:52 +0600

    feat: add dynamic main menu

--- src/components/header/header-main.tsx
+++ src/components/header/header-main.tsx
@@ -1,4 +1,4 @@
-'use client';
+// 'use client';
 
 import Link from 'next/link';
 import { UserIcon } from '../icons/user';
@@ -7,49 +7,56 @@ import { SearchIcon } from '../icons/search';
 import { AlignIcon } from '../icons/align';
 import * as Dialog from '@radix-ui/react-dialog';
 import { Cross } from '../icons/cross';
+import { MainMenu } from '@/components/main-menu';
+import { Suspense } from 'react';
 
 export const HeaderMain = () => {
   return (
-    <div className="py-6">
-      {/* <form className="absolute w-80 p-5 shadow rounded right-0 top-full">
-        <div className="relative">
-          <SearchIcon className="absolute top-1/2 left-4 -translate-y-1/2 fill-black/40" />
-          <input
-            className="bg-[#F0F0F0] w-full placeholder:text-black/40 rounded-full text-sm lg:text-base lg:pl-[52px] pl-12 py-3 pr-4 text-black outline-none"
-            type="search"
-            placeholder="Search your products..."
-          />
-        </div>
-      </form> */}
+    <div>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* Logo */}
           <div className="flex items-center">
-            <button className="me-4 mt-1 inline-block lg:hidden">
-              <AlignIcon className="fill-black" />
-            </button>
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <button className="me-4 mt-1 inline-block lg:hidden">
+                  <AlignIcon className="fill-black" />
+                </button>
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-overlayShow" />
+                <Dialog.Content className="fixed top-0 left-0 z-50 h-full w-3/4 bg-white shadow-lg transition data-[state=closed]:-translate-x-full data-[state=closed]:animate-slide-out-from-right data-[state=closed]:duration-300 data-[state=open]:animate-slide-in-from-right data-[state=open]:duration-500 sm:max-w-sm">
+                  <Dialog.Title className="mt-0.5 p-5 font-integral-bold text-3xl leading-[1] text-black">
+                    NextShop
+                  </Dialog.Title>
+
+                  <Dialog.Close asChild>
+                    <button className="absolute top-4 right-4 text-black">
+                      <Cross />
+                    </button>
+                  </Dialog.Close>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
+
             <h1 className="-mt-0.5 font-integral-bold text-3xl leading-[1] text-black">
               <Link href="/">NextShop</Link>
             </h1>
           </div>
 
           {/* Navigation */}
-          <nav className="ms-10 hidden items-center lg:flex">
-            <ul className="flex items-center gap-x-6 text-base text-black">
-              <li>
-                <Link href="/shop">Shop</Link>
-              </li>
-              <li>
-                <Link href="/shop">On Sale</Link>
-              </li>
-              <li>
-                <Link href="/shop">New Arrivals</Link>
-              </li>
-              <li>
-                <Link href="/shop">Brands</Link>
-              </li>
-            </ul>
-          </nav>
+          <Suspense
+            fallback={
+              <div className="ms-10 flex items-center gap-x-4 py-9">
+                <div className="h-5 w-14 animate-pulse rounded bg-gray-200"></div>
+                <div className="h-5 w-14 animate-pulse rounded bg-gray-200"></div>
+                <div className="h-5 w-14 animate-pulse rounded bg-gray-200"></div>
+                <div className="h-5 w-14 animate-pulse rounded bg-gray-200"></div>
+              </div>
+            }
+          >
+            <MainMenu />
+          </Suspense>
 
           {/* Product Search */}
           <div className="ms-10 hidden flex-1 bg-amber-50 lg:block">
@@ -74,8 +81,8 @@ export const HeaderMain = () => {
                 </button>
               </Dialog.Trigger>
               <Dialog.Portal>
-                <Dialog.Overlay className="fixed inset-0 bg-black/60 data-[state=open]:animate-overlayShow" />
-                <Dialog.Content className="fixed top-0 left-0 w-full bg-white px-4 pt-12 pb-10 data-[state=open]:animate-overlayShow">
+                <Dialog.Overlay className="fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-overlayShow" />
+                <Dialog.Content className="fixed top-0 left-0 z-50 w-full bg-white px-4 pt-12 pb-10 data-[state=open]:animate-overlayShow">
                   <Dialog.Title></Dialog.Title>
                   <form className="">
                     <div className="relative">
---
+++ src/lib/app-fetch.ts
@@ -0,0 +1,10 @@
+export function apiFetch(url: string, config: RequestInit = {}) {
+  return fetch(`${process.env.NEXT_PUBLIC_WOOCOMMERCE_SITE_URL}/wp-json${url}`, {
+    ...config,
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Basic ' + btoa(`${process.env.STIE_USERNAME}:${process.env.APPLICATION_PASSWORD}`),
+      ...config.headers,
+    },
+  });
+}
